7f06c70d1706925dc75c5c6b38dcf669f09b144f
ys4ber <youssefsaber1337@gmail.com>
2025-09-02 11:10:45 +0100

    now we can create all the talbes

--- ex03/automatic_table.py
+++ ex03/automatic_table.py
@@ -17,48 +17,57 @@ PORT = os.getenv("POSTGRES_PORT")
 CUSTOMER_PATH = "../subject/customer/"
 
 
-
-def Create_Table(conn, titles):
-    with conn.cursor() as cur:
-        create_table_query = """
-            CREATE TABLE data_2022_dec (
-                event_time TIMESTAMPTZ NOT NULL,
-                event_type VARCHAR(50),
-                product_id INT,
-                price NUMERIC(10,2),
-                user_id BIGINT,
-                user_session UUID
-            );
-        """
-        cur.execute(create_table_query)
-        print("Table 'data_2022_dec' created successfully.")
-        conn.commit()
-        cur.close()
-        conn.close()
-
-
 def get_titles_from_csv(file_path):
 
     """
-    this function supposed to get the titles from the csv files
+    this function used to get the titles from the csv files
     this will help create the database table properly
     """
 
     titles = []
-
     with open(file_path, "r") as f:
         titles = f.readline().strip().split(",")
         print(f"Getting titles from {file_path}")
-
     return titles
 
+def Create_Table(conn, titles, month, cur):
+    create_table_query = """
+        CREATE TABLE {0} (
+            {1} TIMESTAMPTZ NOT NULL,
+            {2} VARCHAR(50),
+            {3} INT,
+            {4} NUMERIC(10,2),
+            {5} BIGINT,
+            {6} UUID
+        );
+    """.format(month, *titles)
+    cur.execute(create_table_query)
+    conn.commit()
 
 def main():
-    files = subprocess.check_output(['ls', CUSTOMER_PATH]).decode('utf-8').replace(".csv", "").splitlines()
-    print(files)
-    titles = get_titles_from_csv(os.path.join(CUSTOMER_PATH, files[0] + ".csv"))
+    try:
+        files = subprocess.check_output(['ls', CUSTOMER_PATH]).decode('utf-8').replace(".csv", "").splitlines()
+        titles = get_titles_from_csv(os.path.join(CUSTOMER_PATH, files[0] + ".csv"))
 
-    print(titles)
+        conn = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=HOST,
+            port=PORT
+        )
+
+        with conn.cursor() as cur:
+            for month in files:
+                Create_Table(conn, titles, month, cur)
+                print(f"Table created for {month}")
+                time.sleep(2)
+            
+            cur.close()
+            conn.close()
+
+    except Exception as e:
+        print(f"Error getting titles from CSV: {e}")
 
 if __name__ == "__main__":
     main()
